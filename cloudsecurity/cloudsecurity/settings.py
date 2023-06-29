9811512a513a33705f85ffea92215fe088351c18
Brunoiit <francisco18car@gmail.com>
2023-06-28 22:03:45 -0500

    Definiendo los scrapeos

--- cloudsecurity/cloudsecurity/settings.py
+++ cloudsecurity/cloudsecurity/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import scrapy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +34,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'securtyapp'
+    'securtyapp',
+    'scrapy',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

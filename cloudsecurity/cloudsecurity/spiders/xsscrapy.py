5ee976811ced085408dc2b3459c44fbf397f8c81
Brunoiit <francisco18car@gmail.com>
2023-06-30 23:25:01 -0500

    funcional

--- cloudsecurity/cloudsecurity/spiders/xsscrapy.py
+++ cloudsecurity/cloudsecurity/spiders/xsscrapy.py
@@ -1,15 +1,16 @@
 import scrapy
 
 class MySpider(scrapy.Spider):
-    name = 'cloudspider'
-    start_urls = ['http://quotes.toscrape.com/']
+  name = 'cloudspider'
+  start_urls = ['http://quotes.toscrape.com/']
 
-    # Definir la salida como archivo JSON
-    custom_settings = {
-      'FEED_URI' : 'output.json',
-      'FEED_FORMAT' : 'json',
-    }
-def parse(self, response):
-  yield {
-    'title': response.css('title::text').get()
+  # Definir la salida como archivo JSON
+  custom_settings = {
+    'FEED_URI' : 'output.json',
+    'FEED_FORMAT' : 'json',
   }
+  
+  def parse(self, response):
+    yield {
+      'title': response.css('title::text').get()
+    }

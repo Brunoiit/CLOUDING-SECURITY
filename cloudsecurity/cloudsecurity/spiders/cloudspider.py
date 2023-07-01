9bf090353a63080c4cd90f879ec4ee2a9c6333f1
Brunoiit <francisco18car@gmail.com>
2023-06-30 23:57:25 -0500

    scrapy funciona

--- cloudsecurity/cloudsecurity/spiders/cloudspider.py
+++ cloudsecurity/cloudsecurity/spiders/cloudspider.py
@@ -1,18 +1,35 @@
 import scrapy
+from cloudsecurity.spiders.xsscrapy import XSSSpider
 
-class MySpider(scrapy.Spider):
+class CloudSpider(XSSSpider):
     name = 'cloudspider'
-    start_urls = ['http://quotes.toscrape.com/']
+
+    def start_requests(self):
+        # Obtener la URL proporcionada por el usuario
+        url = input("Ingrese la URL a escrapear: ")
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         # Extraer enlaces
         for link in response.css('a::attr(href)').getall():
-            yield scrapy.Request(response.urljoin(link), callback=self.parse)
+            yield {
+                'tag': 'a',
+                'href': link,
+                'html': response.css('a[href="' + link + '"]').get(),
+            }
 
         # Extraer contenidos de formulario
         for form in response.css('form'):
             yield {
+                'tag': 'form',
                 'action': form.css('::attr(action)').get(),
                 'method': form.css('::attr(method)').get(),
                 'inputs': form.css('input::attr(name)').getall(),
+                'html': form.get(),
             }
+
+        # Extraer todo el HTML de la página
+        yield {
+            'tag': 'html',
+            'html': response.body.decode('utf-8'),
+        }
--- cloudsecurity/cloudsecurity/spiders/xsscrapy.py
+++ cloudsecurity/cloudsecurity/spiders/xsscrapy.py
@@ -1,16 +1,31 @@
 import scrapy
 
-class MySpider(scrapy.Spider):
-  name = 'cloudspider'
-  start_urls = ['http://quotes.toscrape.com/']
+class XSSSpider(scrapy.Spider):
+    name = 'xsspider'
 
-  # Definir la salida como archivo JSON
-  custom_settings = {
-    'FEED_URI' : 'output.json',
-    'FEED_FORMAT' : 'json',
-  }
-  
-  def parse(self, response):
-    yield {
-      'title': response.css('title::text').get()
-    }
+    def start_requests(self):
+        # Obtener la URL proporcionada por el usuario
+        url = input("Ingrese la URL a escrapear: ")
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # Aquí se ejecutará la detección de SQL Injection utilizando xsscrapy
+        # Puedes personalizar los parámetros y opciones según tus necesidades
+
+        # Ejemplo de detección de SQL Injection básica
+        xss_payloads = ['\' OR 1=1 --', '\' OR \'a\'=\'a']
+        for payload in xss_payloads:
+            vuln_url = response.url + payload
+            yield scrapy.Request(url=vuln_url, callback=self.parse_result)
+
+    def parse_result(self, response):
+        # Aquí puedes procesar y analizar la respuesta para identificar si hay indicios de SQL Injection
+        # Puedes usar expresiones regulares o patrones específicos para buscar señales de una vulnerabilidad
+
+        # Ejemplo: Buscar la cadena "error" en la respuesta
+        if 'error' in response.text.lower():
+            yield {
+                'vulnerability': 'SQL Injection',
+                'url': response.url,
+                'response': response.text
+            }

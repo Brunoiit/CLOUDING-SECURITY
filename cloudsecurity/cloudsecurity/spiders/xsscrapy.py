c5a6b3f86746f259b8019100f33ab50456b4c183
Brunoiit <francisco18car@gmail.com>
2023-06-30 23:12:08 -0500

    funcional total

--- cloudsecurity/cloudsecurity/spiders/xsscrapy.py
+++ cloudsecurity/cloudsecurity/spiders/xsscrapy.py
@@ -9,3 +9,7 @@ class MySpider(scrapy.Spider):
       'FEED_URI' : 'output.json',
       'FEED_FORMAT' : 'json',
     }
+def parse(self, response):
+  yield {
+    'title': response.css('title::text').get()
+  }

8b3f5df562e42e57e855c719252e883612249379
Brunoiit <francisco18car@gmail.com>
2023-06-30 22:25:57 -0500

    Mejora

--- cloudspider.py
+++ cloudspider.py
@@ -11,3 +11,12 @@ class WebSpider(scrapy.Spider):
         # Yield a new Request for each URL
         for url in urls:
             yield scrapy.Request(url, callback=self.parse)
+
+class MySpider(scrapy.Spider):
+    name = "my_spider"
+
+    def start_requests(self):
+        with open('urls.txt', 'r') as f:
+            urls = f.read().splitlines()
+        for url in urls:
+            yield scrapy.Request(url, callback=self.parse)
--- xsscrapy.py
+++
@@ -1,8 +0,0 @@
-class MySpider(scrapy.Spider):
-    name = "my_spider"
-
-    def start_requests(self):
-        with open('urls.txt', 'r') as f:
-            urls = f.read().splitlines()
-        for url in urls:
-            yield scrapy.Request(url, callback=self.parse)

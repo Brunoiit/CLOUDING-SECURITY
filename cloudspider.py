b82628356ba89cc333ae9fd6cbe1dd0892291cf0
Brunoiit <francisco18car@gmail.com>
2023-06-30 22:49:34 -0500

    Modificaciones

--- cloudspider.py
+++ cloudspider.py
@@ -2,9 +2,8 @@ import scrapy
 
 class MySpider(scrapy.Spider):
     name = 'my_spider'
-    
-    start_urls = ['http://www.sapuyes-narino.gov.co/']  # URL de inicio para el rastreo
-    
+    start_urls = ['http://www.sapuyes-narino.gov.co/']
+
     def parse(self, response):
         # Extraer enlaces
         for link in response.css('a::attr(href)').getall():
@@ -17,3 +16,15 @@ class MySpider(scrapy.Spider):
                 'method': form.css('::attr(method)').get(),
                 'inputs': form.css('input::attr(name)').getall(),
             }
+            
+class TextWriterPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('items.txt', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = str(item) + "\n"
+        self.file.write(line)
+        return item
